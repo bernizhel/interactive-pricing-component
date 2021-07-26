c346124b53b8be1ac10c06095c94be8f0b1dfd8d
Danil Kosenko <Detrop.1995@gmail.com>
2021-07-26 17:03:24 +0300

    Initial commit. Webpack configuration, HTML markup.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Interactive pricing component solution
+
+This is a solution to the [Interactive pricing component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8).
+
+## Table of contents
+
+- [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+- [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Vanilla JavaScript.
+- Semantic HTML5 markup.
+- CSS custom properties, flexbox, grid.
+- Sass preprocessor and BEM methodology.
+- Webpack bundle and PostCSS.
+- Mobile-first workflow.
+
+### What I learned
+
+### Useful resources
+
+## Author
+
+- Frontend Mentor - [@bernizhel](https://www.frontendmentor.io/profile/bernizhel)
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
+
+const postcssPlugins = () => {
+    const plugins = [
+        ['autoprefixer']
+    ];
+    if (!isDev) {
+        plugins.push(['cssnano']);
+    }
+    return plugins;
+}
+
+const CSSLoaders = (...additional) => {
+    const loaders = [
+        MiniCSSExtractPlugin.loader,
+        'css-loader',
+        {
+            loader: "postcss-loader",
+            options: {
+                postcssOptions: {
+                    plugins: postcssPlugins(),
+                },
+            },
+        },
+    ];
+    if (additional) {
+        additional.forEach(loader => loaders.push(loader));
+    }
+    return loaders;
+}
+
+const filename = ext => !isDev ? `[name].[contenthash].${ext}` : `[name].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: ['@babel/polyfill', './index.js'],
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'public'),
+    },
+    resolve: {
+        alias: {
+            '@s': path.resolve(__dirname, 'src/styles'),
+            '@i': path.resolve(__dirname, 'src/images'),
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        minimizer: [
+            new CSSMinimizerPlugin(),
+            new TerserWebpackPlugin(),
+        ],
+    },
+    target: isDev ? 'web' : 'browserslist',
+    devServer: {
+        open: false,
+        port: 3000,
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+            favicon: path.resolve(__dirname, 'src/images/favicon-32x32.png'),
+            inject: 'body',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: filename('css'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: CSSLoaders(),
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: CSSLoaders('sass-loader'),
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: ['inline-svg-loader'],
+            },
+        ],
+    },
+};
